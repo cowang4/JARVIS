3b1dc9af9491768e1dca32b01dc6c9207125868b
Greg C <cowang4@yahoo.com>
2013-06-15 18:39:11 +0000

    fixed #2 made multi line tweets possible

--- Tweet.py
+++ Tweet.py
@@ -15,7 +15,14 @@ auth = OAuth(oauth_token, oauth_token_secret, CONSUMER_KEY, CONSUMER_SECRET)
 twitter = Twitter(auth=auth)
 
 if not sys.argv[1] == "":
-	twitter.statuses.update(status=sys.argv[1])
+	text = ""
+	for arg in sys.argv:
+		if arg == "Tweet.py":
+			#comments are cool kids
+			text = text
+		else:
+			text = text + " " + arg
+	twitter.statuses.update(status=text)
 else:
-	print "You need to specify what to Tweet!"
+	print "ERROR:You need to specify what to Tweet!"
 
